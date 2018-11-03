2fef26babcb39ae223001a7ac6d2a32bd7347539
Maurizio Bonani <maurizio.bonani@gmail.com>
2018-11-03 20:49:13 +0100

    Add demo

--- src/__tests__/utils.js
+++
@@ -1,8 +0,0 @@
-import { capitalize } from '../utils'
-
-describe('test utils', function () {
-
-  it('should capitalize a string', () => {
-    expect(capitalize('capitalize me!')).toBe('Capitalize me!')
-  });
-});
